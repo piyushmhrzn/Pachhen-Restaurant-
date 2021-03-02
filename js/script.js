d4dedfcd4eb593d50300fa6a1898e00bba03e0ad
Piyush Maharjan <piyushmaharjan22@gmail.com>
2021-03-02 17:13:17 +0545

    Hamburger Menu fixed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
-const menuIcon = document.querySelector(".hamburger-menu");
-const navbar = document.querySelector(".navbar");
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-list');
+const bar1 = document.querySelector('.line.top');
+const bar2 = document.querySelector('.line.middle');
+const bar3 = document.querySelector('.line.bottom');
+// const links = document.querySelector('.list-group li');
 
-menuIcon.addEventListener("click", () => {
-    navbar.classList.toggle("change"); 
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+    bar1.classList.toggle("active1");
+    bar2.classList.toggle("active2");
+    bar3.classList.toggle("active3");
+    hamburger.classList.toggle("burgerBG");
 });
